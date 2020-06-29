01852bd855e22c570a90829485b6eb63681735bd
Zachary Rener <me@zacharyrener.com>
2020-06-29 00:55:00 +0000

    Switched to a websocket instead of spamming get requests

--- server.js
+++ server.js
@@ -1,27 +1,52 @@
 const fs = require('fs')
 const https = require('https')
 const express = require('express')
-
-
+const socketIo = require("socket.io")
 const app = express()
+let bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', express.static('public'))
 
-app.get('/send', (req, res) => {
+app.post('/paid', (req, res) => {
 
-   
-
-    res.send('sent')
+    const id = req.body.data.id;
+    const amount = req.body.data.amount_satoshi;
+    const paid = req.body.data.paid;
+    const desc = req.body.data.description;
+    console.log(`payment recieved for ${amount} satoshis`)
+    confirmPayment();
 
 })
 
-https.createServer({
+const server = https.createServer({
 
     key: fs.readFileSync('/etc/letsencrypt/live/ln.zacharyrener.com/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/ln.zacharyrener.com/fullchain.pem'),
     ca: fs.readFileSync('/etc/letsencrypt/live/ln.zacharyrener.com/fullchain.pem')
 
-}, app).listen(443, () => {
+}, app)
+
+const io = socketIo(server)
+
+const confirmPayment = () => {
+
+    io.emit('paid', 'Thank you for your payment.');
+
+}
+
+io.on('connection', (socket) => {
+
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+});
+
+server.listen(443, () => {
 
     console.log('Listening...')
 
